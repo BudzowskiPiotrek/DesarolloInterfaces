82dde2305747c624cde2bba34a49dba9b1b4c09d
Piotrek Budzowski <piobudzows@gmail.com>
2025-11-20 20:10:17 +0100

    cambios

--- 02_FabricaHonda/obj/Debug/net8.0/Fabrica.AssemblyInfo.cs
+++ 02_FabricaHonda/obj/Debug/net8.0/Fabrica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fabrica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15ec9744c0125b3f693701787fccf0c102c2226e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f2695ade04d9e0521db1e41e56a47dd3bd64ac3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fabrica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fabrica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
